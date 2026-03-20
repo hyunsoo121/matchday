d087c5f97a266d6e7d58d032c6b68314484ed5d6
ggom121@naver.com <ggom121@naver.com>
2026-03-20 11:48:22 +0900

    feat: init: 초기 세팅

---
+++ src/test/java/com/heksis/matchday/MatchdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heksis.matchday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
