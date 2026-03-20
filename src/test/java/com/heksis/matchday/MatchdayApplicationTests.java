167ec6476425db55103e18c039c22096f533d589
ggom121@naver.com <ggom121@naver.com>
2026-03-20 16:30:59 +0900

    feat: docker-compose 파일 추가

--- src/test/java/com/heksis/matchday/MatchdayApplicationTests.java
+++ src/test/java/com/heksis/matchday/MatchdayApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MatchdayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
